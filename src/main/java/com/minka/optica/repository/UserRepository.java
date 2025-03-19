662a9d8f824f291e5941d1001cfc9ba0287c3ef6
Sebastian Asuncion Montero <desarrollo.espinacas1966@gmail.com>
2025-03-19 19:46:42 +0100

    18/03/2025

---
+++ src/main/java/com/minka/optica/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.minka.optica.repository;
+
+import com.minka.optica.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
